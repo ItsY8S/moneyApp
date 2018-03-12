15952e7178ea8e7fb7702e94f3d0efe66a6e5ddb
Griffin Yates <gyates@fullsail.edu>
2018-03-11 23:49:48 -0400

    Finished project

---
+++ money/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <h1>Budget</h1>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Header
